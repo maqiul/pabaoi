174b9f536a7381a9fd73da8ff266f7411b828f66
maqiuliang <450000964@qq.com>
2020-02-04 09:27:18 +0800

    对接代码更新

---
+++ Checkpic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pcbaoi
+{
+    class Checkpic
+    {
+        private bool isNg;
+        private List<bbox_t> lists;
+
+        public bool IsNg { get => isNg; set => isNg = value; }
+        public List<bbox_t> Lists { get => lists; set => lists = value; }
+    }
+}
