78a47f1deee3299530ccd8629ded53d86d59c2ba
yiningzeng <yiningzeng@outlook.com>
2020-03-03 10:33:41 +0800

    feat: 更新算法调用模块+图片控件

---
+++ Algorithms/AiDetectRect.cs
@@ -0,0 +1,20 @@
+﻿using pcbaoi.Algorithms.Pars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pcbaoi.Algorithms.Model
+{
+    public class AiDetectRect : BaseDetectRect<AiPars>
+    {
+        public override AiPars Run(AiPars res)
+        {
+            int n = detect_image_path(res.fileName, ref res.boxlist);
+            //if (n == -1) Console.WriteLine("调用失败，请检测目录是否包含opencv的dll");
+            return res;
+            //throw new NotImplementedException();
+        }
+    }
+}
