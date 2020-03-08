f3e3903b8275b8f4dbe5dc786ce9f3fa8dd877af
111 <450000964@qq.com>
2020-03-08 11:34:28 +0800

    本地数据库更新

---
+++ Tools/DataGridviewUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pcbaoi.Tools
+{
+    public class DataGridviewUtils
+    {
+        public static int Getrowid(DataGridView dataGridView,int Columnid,string findvalue) {
+            int count = 0;
+            foreach (DataGridViewRow row in dataGridView.Rows) 
+            {
+                if (row.Cells[Columnid].Value.ToString().Equals(findvalue)) {
+                    return count;                
+                }
+                count++;
+            
+            }
+            return -99;
+        
+        
+        
+        }
+    }
+}
